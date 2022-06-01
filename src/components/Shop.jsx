7d7b44c7af86efc79be58e2842d21ac7741eef54
SubhamGupta007 <subhamyoyo23@gmail.com>
2022-06-01 16:38:48 +0530

    added bacis

---
+++ src/components/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+
+       <div className='container'>
+            <button className="btn btn-primary">-</button>
+            Add this item to cart
+            <button className="btn btn-primary">+</button>
+       </div>
+    </div>
+  )
+}
+
+export default Shop
